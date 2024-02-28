2a5b55be8302998dc93da792c934b84b77dfcb81
saul romero <ingsulky@gmail.com>
2024-02-28 15:51:07 -0600

    Artist corutinas y flows

--- app/src/main/java/com/soulqubit/tmdbclient/presentation/artist/ArtistActivity.kt
+++ app/src/main/java/com/soulqubit/tmdbclient/presentation/artist/ArtistActivity.kt
@@ -11,10 +11,19 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.launch
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.soulqubit.tmdbclient.R
+import com.soulqubit.tmdbclient.data.model.artist.Artist
 import com.soulqubit.tmdbclient.databinding.ActivityArtistBinding
 import com.soulqubit.tmdbclient.presentation.di.Injector
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import javax.inject.Inject
 
 class ArtistActivity : AppCompatActivity() {
@@ -29,62 +38,130 @@ class ArtistActivity : AppCompatActivity() {
         (application as Injector).createArtistSubComponent()
             .inject(this)
 
-        artistViewModel= ViewModelProvider(this,factory)
-            .get(ArtistViewModel::class.java)
+        artistViewModel = ViewModelProvider(this, factory)[ArtistViewModel::class.java]
+
+
+        lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            fun onCreateEvent() {
+                Log.d("ARTTAG", "onCreate")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_START)
+            fun onStartEvent() {
+                Log.d("ARTTAG", "onStart")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            fun onResumeEvent() {
+                Log.d("ARTTAG", "onResume")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+            fun onPauseEvent() {
+                Log.d("ARTTAG", "onPause")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+            fun onStopEvent() {
+                Log.d("ARTTAG", "onStop")
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroyEvent() {
+                Log.d("ARTTAG", "onDestroy")
+            }
+        })
+
         initRecyclerView()
+
+        lifecycleScope.launch {
+            // Repetir esta corrutina mientras el estado del ciclo de vida sea STARTED
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                artistViewModel.artistList.collect {
+                    Log.i("ARTTAG", "flow")
+                    displayPopularArtists(it)
+                }
+            }
+        }
+
     }
 
-    private fun initRecyclerView(){
-        Log.i("ARTTAG","artist activity init recycler view")
+
+    private fun initRecyclerView() {
+        Log.i("ARTTAG", "artist activity init recycler view")
         binding.artistRecyclerView.layoutManager = LinearLayoutManager(this)
         adapter = ArtistAdapter()
         binding.artistRecyclerView.adapter = adapter
-        displayPopularArtists()
+
+        GlobalScope.launch(Dispatchers.IO) {
+            artistViewModel.getArtists()
+        }
+
     }
 
-    private fun displayPopularArtists(){
-        Log.i("ARTTAG","artist activity display popular atrtist")
+
+    private fun displayPopularArtists(artistList: List<Artist>?) {
+        Log.i("ARTTAG", "artist activity display popular atrtist")
         binding.artistProgressBar.visibility = View.VISIBLE
-        val responseLiveData = artistViewModel.getArtists()
-        responseLiveData.observe(this, Observer {
-            if(it!=null){
-                Log.i("ARTTAG","observed ${it.toString()}")
-                adapter.setList(it)
-                adapter.notifyDataSetChanged()
-                binding.artistProgressBar.visibility = View.GONE
-            }else{
-                binding.artistProgressBar.visibility = View.GONE
-                Toast.makeText(applicationContext,"No data available", Toast.LENGTH_LONG).show()
-            }
-        })
+        if (artistList != null) {
+            Log.i("ARTTAG", "observed $artistList")
+            adapter.setList(artistList)
+            adapter.notifyDataSetChanged()
+            binding.artistProgressBar.visibility = View.GONE
+        } else {
+            binding.artistProgressBar.visibility = View.GONE
+            Toast.makeText(applicationContext, "No data available", Toast.LENGTH_LONG).show()
+        }
+
     }
 
+
+    /*
+        private fun displayPopularArtists(){
+            Log.i("ARTTAG","artist activity display popular atrtist")
+            binding.artistProgressBar.visibility = View.VISIBLE
+            val responseLiveData = artistViewModel.getArtists()
+            responseLiveData.observe(this, Observer {
+                if(it!=null){
+                    Log.i("ARTTAG","observed ${it.toString()}")
+                    adapter.setList(it)
+                    adapter.notifyDataSetChanged()
+                    binding.artistProgressBar.visibility = View.GONE
+                }else{
+                    binding.artistProgressBar.visibility = View.GONE
+                    Toast.makeText(applicationContext,"No data available", Toast.LENGTH_LONG).show()
+                }
+            })
+        }
+    */
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater : MenuInflater = menuInflater
-        inflater.inflate(R.menu.update,menu)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.update, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId){
+        return when (item.itemId) {
             R.id.action_update -> {
                 updateTvShows()
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
 
     }
 
-    private fun updateTvShows(){
+    private fun updateTvShows() {
         binding.artistProgressBar.visibility = View.VISIBLE
         val response = artistViewModel.updateArtists()
         response.observe(this, Observer {
-            if(it!=null){
+            if (it != null) {
                 adapter.setList(it)
                 adapter.notifyDataSetChanged()
                 binding.artistProgressBar.visibility = View.GONE
-            }else{
+            } else {
                 binding.artistProgressBar.visibility = View.GONE
             }
         })
--- app/src/main/java/com/soulqubit/tmdbclient/presentation/artist/ArtistViewModel.kt
+++ app/src/main/java/com/soulqubit/tmdbclient/presentation/artist/ArtistViewModel.kt
@@ -2,18 +2,34 @@ package com.soulqubit.tmdbclient.presentation.artist
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.soulqubit.tmdbclient.data.model.artist.Artist
 import com.soulqubit.tmdbclient.domain.usecase.GetArtistsUseCase
 import com.soulqubit.tmdbclient.domain.usecase.UpdateArtistsUseCase
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
 
 class ArtistViewModel(
     private val getArtistsUseCase: GetArtistsUseCase,
     private val updateArtistsUseCase: UpdateArtistsUseCase
 ): ViewModel() {
 
+    private val _artistList = MutableSharedFlow<List<Artist>?>()
+    val artistList : SharedFlow<List<Artist>?> = _artistList
+/*
     fun getArtists() = liveData {
         val artistList = getArtistsUseCase.execute()
         emit(artistList)
     }
+*/
+    suspend fun getArtists() {
+        viewModelScope.launch {
+            _artistList.emit(getArtistsUseCase.execute())
+
+        }
+    }
+
 
     fun updateArtists() = liveData {
         val artistList = updateArtistsUseCase.execute()
